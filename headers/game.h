bf54174a3dc8d41bb641f10af5b9f3b676e79287
ZenThe0 <adrien.doan@insa-lyon.fr>
2024-11-08 10:41:02 +0100

    Change to Game*

--- headers/game.h
+++ headers/game.h
@@ -4,22 +4,22 @@
 typedef struct Game
 {
     int id;
-    Player player1;
-    Player player2;
-    Board board;
+    Player* player1;
+    Player* player2;
+    Board* board;
     int score[2];
     int clockwise;
     int currentPlayer;
 } Game;
 
-Game initGame(Player, Player);
-void displayBoard(Game);
-void addPointScore(Game, int index, int points);
-void chooseHouse(Game, int houseNb);
-void attributePoints(Game);
-int updateBoard(Game); //retour = nb de points à attribuer au joueur
-int isGameOver(Game);
-Player getWinner(Game);
+Game* initGame(Player*, Player*);
+void displayBoard(Game*);
+void addPointScore(Game*, int index, int points);
+void chooseHouse(Game*, int houseNb);
+void attributePoints(Game*);
+int updateBoard(Game*); //retour = nb de points à attribuer au joueur
+int isGameOver(Game*);
+Player getWinner(Game*);
 
 
 
