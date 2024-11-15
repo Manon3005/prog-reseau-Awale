6f5b2db0a33508a50fabd1880d675bfe8346d184
ZenThe0 <adrien.doan@insa-lyon.fr>
2024-11-15 09:36:45 +0100

    creating signatures csvManagment

--- server/src/csvManager.c
+++ server/src/csvManager.c
@@ -2,3 +2,15 @@
 #include <stdio.h>
 
 #include "../headers/csvManager.h"
+
+int isCLientInCsv(csvManager* csvManager, char* username){
+
+}
+
+int authenticateClient(csvManager* csvManager, char* username, char* pwd){
+
+}
+
+int addClientCsv(csvManager* csvManager, char* username, char* pwd){
+    
+}
